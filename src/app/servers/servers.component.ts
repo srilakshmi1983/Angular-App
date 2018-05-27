0446e4e09e5bf850c3b2690788797468461623bb
srilaxmiA <srilakshmi.a1983@gmail.com>
2018-05-27 13:58:07 +0100

    New Method

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  allowUserInput = 'Provide your name!';
 
   constructor() { 
     setTimeout (() => {
@@ -17,4 +19,11 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created'
+  }
+
+  onUserInput(){
+    this.allowUserInput = 'Input accepted and processed'
+  }
 }
